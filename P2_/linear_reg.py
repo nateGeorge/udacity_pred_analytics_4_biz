40bee8ea9e6a2a610b85ebce8f425c3d9bdc62f7
Nate George <wordsforthewise@gmail.com>
2016-12-16 12:50:26 -0700

    CLN: remove wrong folder

--- P2_/linear_reg.py
+++
@@ -1,6 +0,0 @@
-from statsmodels.regression.linear_model import OLS
-import statsmodels.api as sm
-import pandas as pd
-import matplotlib.pyplot as plt
-
-dem = pd.read_csv('p2-wy-demographic-data.csv')
