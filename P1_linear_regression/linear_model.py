0c157de2d4cf33f55ed0c02cc2cebb65d115fbc8
wordsforthewise <wordsforthewise@gmail.com>
2016-12-17 18:15:10 -0700

    FEAT: fix P1, rename P2

--- P1_linear_regression/linear_model.py
+++ P1_linear_regression/linear_model.py
@@ -7,7 +7,12 @@ from collections import OrderedDict
 current = pd.read_excel('p1-customers.xlsx')
 new = pd.read_excel('p1-mailinglist.xlsx')
 
-current['profit'] = current['Avg Sale Amount'] * 0.5 - 6.5
+# the instructions were not clear here.  Do we fit a regression to profit,
+# or gross revenue?  I'm guessing from my first failing review, it's supposed
+# to be a fit to gross revenue??
+#current['profit'] = current['Avg Sale Amount'] * 0.5 - 6.5
+# it's also not clear if revenue is just avg sale amount, or avg sale amt * avg num products purchased
+current['revenue'] = current['Avg Sale Amount']
 
 cust_conv_dict = OrderedDict()
 cust_conv_dict['Store Mailing List'] = 0
@@ -22,10 +27,14 @@ new['loyalty_level'] = new['Customer Segment'].apply(lambda x: cust_conv_dict[x]
 # plt.show()
 
 pred_vars = ['loyalty_level', 'Avg Num Products Purchased', '# Years as Customer']
+# looks like 'store mailing list' should be 0, and 'credit card only' should be 1,
+# if it was to be a linear relationship with CC/mailing list, etc
+# looks like num years customer doesn't matter
+
 for p in pred_vars:
-    current.plot(x=p, y='profit', kind='scatter')
+    current.plot(x=p, y='revenue', kind='scatter', alpha=0.5)
     plt.xlabel(p)
-    plt.ylabel('profit')
+    plt.ylabel('revenue')
     if p == 'loyalty_level':
         plt.xticks(list(cust_conv_dict.values()), list(cust_conv_dict.keys()),
                     rotation=10)
@@ -33,43 +42,60 @@ for p in pred_vars:
 
 current = pd.get_dummies(data=current, columns=['Customer Segment'], prefix='', prefix_sep='')
 new = pd.get_dummies(data=new, columns=['Customer Segment'], prefix='', prefix_sep='')
-# looks like 'store mailing list' should be 0, and 'credit card only' should be 1.
-# looks like num years customer doesn't matter
+# need to drop 'Credit Card Only' so the coefficient will be 0 for that variable
+# essentially, the coefficient for Credit Card Only will be rolled into the intercept
+current.drop('Credit Card Only', inplace=True, axis=1)
+new.drop('Credit Card Only', inplace=True, axis=1)
 
-X = current[['loyalty_level',
-            'Avg Num Products Purchased',
-            '# Years as Customer']]
-X = sm.add_constant(X)  # Adds a constant term to the predictor
+def fit_3_vars(current):
+    # try a fit with # years customer in there to show the R2 is worse
+    # takes the 'current' dataframe as an argument
+    X = current[['loyalty_level',
+                'Avg Num Products Purchased',
+                '# Years as Customer']]
+    X = sm.add_constant(X)  # Adds a constant term to the predictor
 
-model = OLS(current['profit'], X)
-res = model.fit()
-print(res.summary())
+    model = OLS(current['revenue'], X)
+    res = model.fit()
+    print(res.summary())
 
-X = current[['loyalty_level',
-            'Avg Num Products Purchased']]
-#X = sm.add_constant(X)  # Adds a constant term to the predictor
-model = OLS(current['profit'], X)
-res = model.fit()
-preds = res.predict(X)
-print(res.summary())
+def fit_loyalty_level(current):
+    # this fit, with loyalty level as a singe variable, actually fits the data best
+    # takes the 'current' dataframe as an argument
+    X = current[['loyalty_level',
+                'Avg Num Products Purchased']]
+    #X = sm.add_constant(X)  # Adds a constant term to the predictor
+    model = OLS(current['revenue'], X)
+    res = model.fit()
+    preds = res.predict(X)
+    print(res.summary())
 
-plt.scatter(X.iloc[:, 1], current['profit'], alpha=0.5, c='r')
-plt.scatter(X.iloc[:, 1], preds, alpha=0.5, c='b')
-plt.show()
+# for examining a fit
+# plt.scatter(X.iloc[:, 1], current['revenue'], alpha=0.5, c='r')
+# plt.scatter(X.iloc[:, 1], preds, alpha=0.5, c='b')
+# plt.show()
 
-X = current[['Credit Card Only', 'Loyalty Club Only',
+# execute the main fit
+X = current[['Loyalty Club Only',
             'Loyalty Club and Credit Card', 'Store Mailing List',
             'Avg Num Products Purchased']]
 X = sm.add_constant(X)  # Adds a constant term to the predictor
-model = OLS(current['profit'], X)
+model = OLS(current['revenue'], X)
 res = model.fit()
 preds = res.predict(X)
 print(res.summary())
 
-new_X = new[['Credit Card Only', 'Loyalty Club Only',
+# predict revenue from customers
+new_X = new[['Loyalty Club Only',
             'Loyalty Club and Credit Card', 'Store Mailing List',
             'Avg Num Products Purchased']]
 new_X = sm.add_constant(new_X)
 new_preds = res.predict(new_X)
 
-print(sum(new_preds * new['Score_Yes']))
+# calculate expected profit.
+
+expected_revenue = new_preds * new['Score_Yes']
+expected_profit = expected_revenue * 0.5 - 6.5
+total_exp_profit = sum(expected_profit)
+
+print('total expected profit:', total_exp_profit)
